3020caa9bcc33d2cfeb8c73c47986c93a4ac98a0
Peng Zhong <peng@nylira.com>
2017-01-18 11:15:57 +0900

    init

---
+++ src/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <textarea v-if="inputType === 'textarea'"
+    class="inputClass"
+    :value="inputValue"
+    :placeholder="inputPlaceholder">
+  </textarea>
+  <input v-else
+    :class="inputClass"
+    :value="inputValue"
+    :placeholder="inputPlaceholder"
+  >
+</template>
+
+<script>
+export default {
+  name: 'pz-button',
+  computed: {
+    inputClass () {
+      let value = 'pz-input'
+      if (this.inputSize === 'large') value += ' pz-input-large'
+      if (this.inputSize === 'small') value += ' pz-input-small'
+      if (this.inputStyle === 'tendermint') value += ' pz-style-tendermint'
+      return value
+    },
+  },
+  props: ['input-value', 'input-placeholder', 'input-type', 'input-size', 'input-style']
+}
+</script>
+
+<style src="./style.css"></style>
